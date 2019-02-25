abd59ee1d9d4fab36970d055ccd26ac382edb990
Josh Abraham <sinisterpatrician@gmail.com>
2019-02-24 20:09:28 -0500

    Use iterator correctly in bin

--- src/bin.rs
+++ src/bin.rs
@@ -18,11 +18,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let mut journal = Journal::new(&args[1]).unwrap();
     
-    let n_entries = journal.header.n_objects;
-
-    let mut obj_iter = ObjectIter::new(&mut journal).unwrap();
-    for _ in 0..n_entries {
-        let obj = obj_iter.next().expect("object iterator error");
+    let obj_iter = ObjectIter::new(&mut journal).unwrap();
+    for obj in obj_iter {
         if let Object::Data(d) = obj {
             println!("type: {:?} size: {}", d.object.type_, d.object.size);
             println!("Payload: {:?}", show(&d.payload));
@@ -30,7 +27,7 @@ fn main() {
     }
 
     //let mut obj_iter = ObjectHeaderIter::new(&mut journal).unwrap();
-    //for _ in 0..n_entries {
+    //for in obj_iter {
     //    let oh = obj_iter.next().expect("object iterator error");
     //    if oh.type_ == ObjectType::ObjectData {
     //        println!("type: {:?} size: {}", oh.type_, oh.size);
