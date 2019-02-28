3d8771e3c0a0f884e481590d2954afda76c30759
Josh Abraham <sinisterpatrician@gmail.com>
2019-02-28 16:28:58 -0500

    Add tests for system log file

--- tests/test.rs
+++ tests/test.rs
@@ -64,4 +64,55 @@ use memmap::Mmap;
         assert_eq!(counter, expected);
     }
 
+    #[test]
+    fn test_object_header_iter_system() {
+        let mut file = File::open("tests/system.journal").unwrap();
+        let mmap = unsafe { Mmap::map(&file).expect("mmap err") };
+        let buf = &*mmap;
+        let mut journal = Journal::new(buf).unwrap();
+        let expected = journal.header.n_objects;
+        let mut obj_iter = ObjectHeaderIter::new(&mut journal).unwrap();
+        let mut counter = 0;
+        for oh in obj_iter {
+            if oh.type_ == ObjectType::ObjectData {
+                println!("type: {:?} size: {}", oh.type_, oh.size);
+            }
+            counter += 1;
+        }
+        assert_eq!(counter, expected);
+    }
+
+    #[test]
+    fn test_object_iter_system() {
+
+        let mut file = File::open("tests/system.journal").unwrap();
+        let mmap = unsafe { Mmap::map(&file).expect("mmap err") };
+        let buf = &*mmap;
+        let c = Cell::new(buf);
+        let mut journal = Journal::new(buf).unwrap();
+        let expected = journal.header.n_objects;
+        let mut counter = 0;
+        let mut obj_iter = ObjectIter::new(&mut journal).unwrap();
+        for obj in obj_iter {
+            counter += 1;
+        }
+        assert_eq!(counter, expected);
+    }
+
+    #[test]
+    fn test_entry_iter_system() {
+
+        let mut file = File::open("tests/system.journal").unwrap();
+        let mmap = unsafe { Mmap::map(&file).expect("mmap err") };
+        let buf = &*mmap;
+        let c = Cell::new(buf);
+        let mut journal = Journal::new(buf).unwrap();
+        let expected = journal.header.n_entries;
+        let mut counter = 0;
+        let mut ent_iter = EntryIter::new(&mut journal).unwrap();
+        for ent in ent_iter {
+            counter += 1;
+        }
+        assert_eq!(counter, expected);
+    }
 }
