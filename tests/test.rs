a9808915bdd25cf8c092d1648d3ecbef60e753d5
Josh Abraham <sinisterpatrician@gmail.com>
2019-03-03 20:00:23 -0500

    Cleanup tests

--- tests/test.rs
+++ tests/test.rs
@@ -2,16 +2,14 @@
 mod tests {
 use journald::*;
 use std::fs::File;
-use std::cell::Cell;
 use memmap::Mmap;
 
     #[test]
     fn test_compression_false() {
-        let mut file = File::open("tests/user-1000.journal").unwrap();
+        let file = File::open("tests/user-1000.journal").unwrap();
         let mmap = unsafe { Mmap::map(&file).expect("mmap err") };
         let buf = &*mmap;
-        let c = Cell::new(buf);
-        let mut journal = Journal::new(buf).unwrap();
+        let journal = Journal::new(buf).unwrap();
         let obj_iter = journal.header_iter();
         for oh in obj_iter {
             assert!(!oh.is_compressed());
@@ -22,12 +20,10 @@ use memmap::Mmap;
     fn test_hash_object() {
     use journald::hash::rhash64;
 
-        let mut file = File::open("tests/user-1000.journal").unwrap();
+        let file = File::open("tests/user-1000.journal").unwrap();
         let mmap = unsafe { Mmap::map(&file).expect("mmap err") };
         let buf = &*mmap;
-        let c = Cell::new(buf);
-        let mut journal = Journal::new(buf).unwrap();
-        let expected = journal.header.n_objects;
+        let journal = Journal::new(buf).unwrap();
         let obj_iter = journal.obj_iter();
         for obj in obj_iter {
             if let Object::Data(o) = obj {
@@ -40,11 +36,10 @@ use memmap::Mmap;
     #[test]
     fn test_object_iter_user() {
 
-        let mut file = File::open("tests/user-1000.journal").unwrap();
+        let file = File::open("tests/user-1000.journal").unwrap();
         let mmap = unsafe { Mmap::map(&file).expect("mmap err") };
         let buf = &*mmap;
-        let c = Cell::new(buf);
-        let mut journal = Journal::new(buf).unwrap();
+        let journal = Journal::new(buf).unwrap();
         let expected = journal.header.n_objects;
         let mut counter = 0;
         let obj_iter = journal.obj_iter();
@@ -61,15 +56,14 @@ use memmap::Mmap;
     #[test]
     fn test_entry_iter_user() {
 
-        let mut file = File::open("tests/user-1000.journal").unwrap();
+        let file = File::open("tests/user-1000.journal").unwrap();
         let mmap = unsafe { Mmap::map(&file).expect("mmap err") };
         let buf = &*mmap;
-        let c = Cell::new(buf);
-        let mut journal = Journal::new(buf).unwrap();
+        let journal = Journal::new(buf).unwrap();
         let expected = journal.header.n_entries;
         let mut counter = 0;
         let ent_iter = journal.entry_iter();
-        for ent in ent_iter {
+        for _ in ent_iter {
             counter += 1;
         }
         assert_eq!(counter, expected);
@@ -78,11 +72,10 @@ use memmap::Mmap;
     #[test]
     fn test_multi_iter_user() {
 
-        let mut file = File::open("tests/user-1000.journal").unwrap();
+        let file = File::open("tests/user-1000.journal").unwrap();
         let mmap = unsafe { Mmap::map(&file).expect("mmap err") };
         let buf = &*mmap;
-        let c = Cell::new(buf);
-        let mut journal = Journal::new(buf).unwrap();
+        let journal = Journal::new(buf).unwrap();
         let hdr_iter = journal.header_iter();
         for oh in hdr_iter {
             if oh.type_ == ObjectType::ObjectData {
@@ -90,7 +83,7 @@ use memmap::Mmap;
             }
         }
 
-        let obj_iter = journal.obj_iter();
+        let _obj_iter = journal.obj_iter();
         let entry_iter = journal.entry_iter();
         for entry in entry_iter {
             println!("timestamp: {}", entry.realtime);
@@ -99,10 +92,10 @@ use memmap::Mmap;
 
     #[test]
     fn test_object_header_iter_user() {
-        let mut file = File::open("tests/user-1000.journal").unwrap();
+        let file = File::open("tests/user-1000.journal").unwrap();
         let mmap = unsafe { Mmap::map(&file).expect("mmap err") };
         let buf = &*mmap;
-        let mut journal = Journal::new(buf).unwrap();
+        let journal = Journal::new(buf).unwrap();
         let expected = journal.header.n_objects;
         let obj_iter = journal.header_iter();
         let mut counter = 0;
@@ -117,10 +110,10 @@ use memmap::Mmap;
 
     #[test]
     fn test_object_header_iter_system() {
-        let mut file = File::open("tests/system.journal").unwrap();
+        let file = File::open("tests/system.journal").unwrap();
         let mmap = unsafe { Mmap::map(&file).expect("mmap err") };
         let buf = &*mmap;
-        let mut journal = Journal::new(buf).unwrap();
+        let journal = Journal::new(buf).unwrap();
         let expected = journal.header.n_objects;
         let obj_iter = journal.header_iter();
         let mut counter = 0;
@@ -136,15 +129,14 @@ use memmap::Mmap;
     #[test]
     fn test_object_iter_system() {
 
-        let mut file = File::open("tests/system.journal").unwrap();
+        let file = File::open("tests/system.journal").unwrap();
         let mmap = unsafe { Mmap::map(&file).expect("mmap err") };
         let buf = &*mmap;
-        let c = Cell::new(buf);
-        let mut journal = Journal::new(buf).unwrap();
+        let journal = Journal::new(buf).unwrap();
         let expected = journal.header.n_objects;
         let mut counter = 0;
         let obj_iter = journal.obj_iter();
-        for obj in obj_iter {
+        for _ in obj_iter {
             counter += 1;
         }
         assert_eq!(counter, expected);
@@ -153,15 +145,14 @@ use memmap::Mmap;
     #[test]
     fn test_entry_iter_system() {
 
-        let mut file = File::open("tests/system.journal").unwrap();
+        let file = File::open("tests/system.journal").unwrap();
         let mmap = unsafe { Mmap::map(&file).expect("mmap err") };
         let buf = &*mmap;
-        let c = Cell::new(buf);
-        let mut journal = Journal::new(buf).unwrap();
+        let journal = Journal::new(buf).unwrap();
         let expected = journal.header.n_entries;
         let mut counter = 0;
         let ent_iter = journal.entry_iter();
-        for ent in ent_iter {
+        for _ in ent_iter {
             counter += 1;
         }
         assert_eq!(counter, expected);
