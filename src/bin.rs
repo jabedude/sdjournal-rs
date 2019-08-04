971270fe77a4f200511b1c449c9291956914eb45
Josh Abraham <sinisterpatrician@gmail.com>
2019-08-04 08:44:46 -0400

    bin: remove all unwraps

--- src/bin.rs
+++ src/bin.rs
@@ -6,23 +6,24 @@ use chrono::prelude::DateTime;
 use chrono::{Utc};
 use std::time::{UNIX_EPOCH, Duration};
 use std::io::Write;
+use std::io::{Error, ErrorKind};
 
 // TODO: work on entrt struct to allow for propper formatting of entries
 
 
-fn main() {
+fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
 
     // TODO: going to need to handle command line flags...
     if args.len() != 2 {
         println!("Usage: {} <journal file>", args[0]);
-        return;
+        return Err(Error::new(ErrorKind::InvalidInput, "Needs at least one argument"));
     }
 
-    let file = File::open(&args[1]).unwrap();
+    let file = File::open(&args[1])?;
     let mmap = unsafe { Mmap::map(&file).expect("mmap err") };
     let buf = &*mmap;
-    let journal = Journal::new(buf).unwrap();
+    let journal = Journal::new(buf)?;
     
     //Iterate over all entry objects
     for ent in journal.iter_entries() {
@@ -32,18 +33,20 @@ fn main() {
         print!("{} ", datetime.format("%b %d %H:%M:%S"));
         for obj in ent.items {
             //println!("obj: {}", obj.object_offset);
-            let data = match get_obj_at_offset(buf, obj.object_offset).unwrap() {
+            let data = match get_obj_at_offset(buf, obj.object_offset)? {
                 Object::Data(d) => d,
                 _ => continue,
             };
 
             if data.payload.starts_with(b"SYSLOG_IDENTIFIER") {
-                std::io::stdout().write_all(&data.payload[18..]).unwrap();
+                std::io::stdout().write_all(&data.payload[18..])?;
             } else if data.payload.starts_with(b"MESSAGE") {
-                std::io::stdout().write_all(&data.payload[7..]).unwrap();
-                std::io::stdout().write_all(b"\n").unwrap();
+                std::io::stdout().write_all(&data.payload[7..])?;
+                std::io::stdout().write_all(b"\n")?;
             }
 
         }
     }
+
+    Ok(())
 }
