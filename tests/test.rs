b5fd8af81c81fd7c3b90c26e8e32b2c8060c1af5
Josh Abraham <sinisterpatrician@gmail.com>
2019-08-05 21:32:17 -0400

    Remove unused import

--- tests/test.rs
+++ tests/test.rs
@@ -3,7 +3,7 @@ mod tests {
     use journald::*;
     use memmap::Mmap;
     use std::fs::File;
-    use pretty_assertions::{assert_eq, assert_ne};
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn test_journal_state_offline() {
